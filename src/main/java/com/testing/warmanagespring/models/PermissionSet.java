0796cd662ea81fbb01b5839c4858915579025059
Chris Rinaldi <crinaldi44@gmail.com>
2023-09-14 21:20:56 -0400

    PermissionSet fields

--- src/main/java/com/testing/warmanagespring/models/PermissionSet.java
+++ src/main/java/com/testing/warmanagespring/models/PermissionSet.java
@@ -1,6 +1,7 @@
 package com.testing.warmanagespring.models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="permission_sets")
@@ -9,6 +10,25 @@ public class PermissionSet {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(unique = true)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public PermissionSet() {
 
